60f7d8c4e74702d8234ad6031296a80874fdd211
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-06-14 20:33:34 -0600

    add types for config

--- lua/onemap/config.lua
+++ lua/onemap/config.lua
@@ -1,9 +1,17 @@
+---@alias Update {lhs: string, buffer_only: boolean, changed_group: string}
+
 ---@class Config
 local config = {
-    additional_register = function() end,
+    ---@type table<number, string>
     groups = {},
+    ---@type table<number, string>
     buffer_local_groups = {},
+    ---@type string
     group_prefix = '__',
+    ---@param update Update
+    on_register = function(update) end,
+    ---@param update Update
+    on_deregister = function(update) end,
 }
 
 return config
